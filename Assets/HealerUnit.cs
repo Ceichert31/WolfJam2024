192f3c799afb69c712fca37b6d7f1af498f32388
zaxnathanson <zaxliyen@gmail.com>
2024-11-16 18:42:33 -0500

    HealerUnit

---
+++ Assets/HealerUnit.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class HealerUnit : ShipUnit
+{
+    Health targetHealthScript;
+    [SerializeField] int healingAmount;
+    [SerializeField] float healingFrequency;
+    bool canHeal = true;
+    float timer;
+    bool canFindNewHealthScript = false;
+    [SerializeField] GameObject healParticles;
+
+    void Start()
+    {
+        GetHealthScript();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canHeal)
+        {
+            timer += Time.deltaTime;
+            if (timer > healingFrequency)
+            {
+                targetHealthScript.AddHealth(healingAmount);
+                if (healParticles != null)
+                {
+                    Instantiate(healParticles, transform.position, Quaternion.identity);
+                }
+                timer = 0;
+            }
+        }
+
+        if (targetHealthScript.CurrentHealth <= 0)
+        {
+            canHeal = false;
+        }
+
+
+        if (MyShipUnitState == ShipUnitState.Detatched)
+        {
+            canFindNewHealthScript = true;
+        }
+
+        if (MyShipUnitState == ShipUnitState.Attached && canFindNewHealthScript == true)
+        {
+            Debug.Log("gotNewHealth");
+            GetHealthScript();
+            canFindNewHealthScript = false;
+        }
+    }
+
+    void GetHealthScript()
+    {
+        targetHealthScript = transform.parent.parent.GetComponent<Health>();
+    }
+}
