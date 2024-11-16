86bc0f4693b73a801452d34f6a323fe3ea673027
sridds <seth.riddensdale@mymail.champlain.edu>
2024-11-15 23:22:01 -0500

    Added Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameManager instance;
+
+    // 
+    public enum EGameState
     {
-        
+        Playing,
+        Building,
+        Lose
     }
 
-    // Update is called once per frame
-    void Update()
+    private EGameState gameState;
+
+    private int score;
+
+    // Getters
+    public int Score { get { return score; } }
+    public EGameState GameState { get { return gameState; } }
+
+    // Helper functions
+    public delegate void GameStateChanged(EGameState state);
+    public GameStateChanged OnGameStateChanged;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void UpdateGameState(EGameState newGameState)
+    {
+        gameState = newGameState;
+
+        // calls an event that other scripts can lsiten to and perform logic
+        OnGameStateChanged?.Invoke(gameState);
+    }
+
+    public void UpdateScore(int scoreCount)
+    {
+        score += scoreCount;
     }
 }
