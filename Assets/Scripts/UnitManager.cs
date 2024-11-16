59177a90303d8428399d140c6757c67c36ea38a3
sridds <seth.riddensdale@mymail.champlain.edu>
2024-11-16 01:12:34 -0500

    Push before bed

--- Assets/Scripts/UnitManager.cs
+++ Assets/Scripts/UnitManager.cs
@@ -29,62 +29,6 @@ public class UnitManager : MonoBehaviour
 
         // test
         List<Unit> units = _units[0].GetUnitNeighbors();
-
-        if (units[0] != null)
-        {
-            Debug.Log("Upper Left:" + units[0] + " pos: " + units[0].transform.position);
-        }
-
-        if (units[1] != null)
-        {
-            Debug.Log("Upper Middle:" + units[1] + " pos: " + units[1].transform.position);
-        }
-
-
-        if (units[2] != null)
-        {
-            Debug.Log("Upper Right:" + units[2] + " pos: " + units[2].transform.position);
-        }
-
-        if (units[3] != null)
-        {
-            Debug.Log("Center Left:" + units[3] + " pos: " + units[3].transform.position);
-        }
-
-        if (units[4] != null)
-        {
-            Debug.Log("Center" + units[4] + " pos: " + units[4].transform.position);
-        }
-
-        if (units[5] != null)
-        {
-            Debug.Log("Center Right:" + units[5] + " pos: " + units[5].transform.position);
-        }
-        if (units[6] != null)
-        {
-            Debug.Log("Lower Left:" + units[6] + " pos: " + units[6].transform.position);
-        }
-        if (units[7] != null)
-        {
-            Debug.Log("Lower Middle:" + units[7] + " pos: " + units[7].transform.position);
-        }
-        if (units[8] != null)
-        {
-            Debug.Log("Lower Right:" + units[8] + " pos: " + units[8].transform.position);
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 
     public void AddUnit()
