41fe73659b8b478f1aa0a134106e738ce45e8d53
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2024-11-16 10:21:37 -0500

    Added flamethrower behavior

--- Assets/Scripts/ProjectileSpawner.cs
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ProjectileSpawner : MonoBehaviour
@@ -9,10 +10,45 @@ public class ProjectileSpawner : MonoBehaviour
     [SerializeField] private ProjectileStats projectileStats;
 
     [SerializeField] private bool canRotate;
-    [SerializeField] private float rotationAngle;
+    [SerializeField] private float minRotationZ;
+    [SerializeField] private float maxRotationZ;
+
+    [SerializeField] private float rotationTime;
+    private float currentRotationTime;
+
+    private bool reverseDirection;
+
+    private Coroutine instance;
     void Start()
     {
         SpawnProjectiles(100, 0.3f, projectileStats);
+    }
+    
+    private void Update()
+    {
+        if (instance != null) return;
+
+        if (!reverseDirection)
+            instance = StartCoroutine(RotateTo(new(transform.eulerAngles.x, transform.eulerAngles.y, minRotationZ), new(transform.eulerAngles.x, transform.eulerAngles.y, maxRotationZ)));
+        else
+            instance = StartCoroutine(RotateTo(new(transform.eulerAngles.x, transform.eulerAngles.y, maxRotationZ), new(transform.eulerAngles.x, transform.eulerAngles.y, minRotationZ)));
+
+        Debug.DrawRay(transform.position, transform.forward * 3f, Color.yellow);
+    }
+
+    IEnumerator RotateTo(Vector3 startEuler, Vector3 targetEuler)
+    {
+        float timeElapsed = 0;
+        transform.eulerAngles = startEuler;
+        while (timeElapsed < rotationTime)
+        {
+            timeElapsed += Time.deltaTime;
+            transform.eulerAngles = Vector3.Lerp(startEuler, targetEuler, timeElapsed / rotationTime);
+            yield return null;
+        }
+        transform.eulerAngles = targetEuler;
+        instance = null;
+        reverseDirection = !reverseDirection;
     }
 
     /// <summary>
