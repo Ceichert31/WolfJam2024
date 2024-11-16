449fecf794d8a2fb6b636747f5f3585b53acdcf1
sridds <seth.riddensdale@mymail.champlain.edu>
2024-11-16 16:35:06 -0500

    Game over in progress

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
     static public GameManager Instance;
 
     public Transform Player;
+
+    [SerializeField]
+    private float _slowDownTime = 3.0f;
 
     // 
     public enum EGameState
@@ -19,6 +25,8 @@ public class GameManager : MonoBehaviour
     private float timePlaying;
     private int kills;
     private int connections;
+
+    public bool debugMode = false;
 
     // Getters
     public float TimePlaying { get { return timePlaying; } }
@@ -44,6 +52,9 @@ public class GameManager : MonoBehaviour
 
     public void Update()
     {
+        // auto lose
+        if (debugMode && Input.GetKeyDown(KeyCode.L)) UpdateGameState(EGameState.Lose);
+
         if (gameState == EGameState.Lose) return;
 
         if (gameState == EGameState.Playing)
@@ -72,6 +83,28 @@ public class GameManager : MonoBehaviour
 
         // calls an event that other scripts can lsiten to and perform logic
         OnGameStateChanged?.Invoke(gameState);
+
+        if (gameState == EGameState.Lose) HandleLoseState();
+    }
+
+    private void HandleLoseState()
+    {
+        StartCoroutine(HandleLose());
+    }
+
+    private IEnumerator HandleLose()
+    {
+        float elapsed = 0.0f;
+
+        while(elapsed < _slowDownTime)
+        {
+            elapsed += Time.unscaledDeltaTime;
+
+            Time.timeScale = Mathf.Lerp(1.0f, 0.0f, elapsed / _slowDownTime);
+            yield return null;
+        }
+
+        Time.timeScale = 0.0f;
     }
 
     public void AddToKills()
