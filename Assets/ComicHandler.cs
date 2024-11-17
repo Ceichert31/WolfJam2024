251977b2a533e289273e4cbfd1853387e1cda233
sridds <seth.riddensdale@mymail.champlain.edu>
2024-11-16 21:41:22 -0500

    More Comic work

--- Assets/ComicHandler.cs
+++ Assets/ComicHandler.cs
@@ -34,10 +34,32 @@ public class ComicHandler : MonoBehaviour
 
     private IEnumerator PlayComic()
     {
+        // SHOW PANEL 1
+        yield return new WaitForSeconds(1.0f);
+
         _source.PlayOneShot(_panel1Sound);
 
+        _panel1.color = new Color(_panel1.color.r, _panel1.color.g, _panel1.color.b, 0.0f);
+        _panel1.gameObject.SetActive(true);
+        _panel1.DOFade(1.0f, 0.5f);
+
+        _panel1.transform.position = new Vector3(_panel1.transform.position.x - 3.0f, _panel1.transform.position.y);
+        _panel1.transform.DOMoveX(_panel1.transform.position.x + 3.0f, 0.5f);
+
+        // SHOW PANEL 2
+        yield return new WaitForSeconds(3.0f);
         _source.PlayOneShot(_panel2Sound);
 
+        _panel2.gameObject.SetActive(true);
+        _panel2.transform.localScale = new Vector3(1.4f, 1.4f, 1.0f);
+        yield return _panel2.transform.DOScale(1.0f, 0.2f).SetEase(Ease.Linear).WaitForCompletion();
+        _panel2.transform.DOShakePosition(0.4f, 0.2f, 50);
+
+        // SHOW PANEL 3
+        yield return new WaitForSeconds(3.0f);
+
+        _panel3.gameObject.SetActive(true);
+        _panel3.transform.DOShakePosition(0.4f, 0.6f, 50);
         _source.PlayOneShot(_panel3Sound);
 
         yield return null;
